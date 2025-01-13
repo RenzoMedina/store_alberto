b6e32d0604556f163abb1a543235f24c0c10ea36
RenzoMedina <medinast30@gmail.com>
2025-01-13 00:06:54 -0300

    Fix view index

--- app/views/components/navbar.php
+++ app/views/components/navbar.php
@@ -279,7 +279,7 @@
             </a>
          </li>
 
-
+<!----- Temporalmente desactivado----
          <li>
             <a href="/login"
                class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
@@ -292,6 +292,7 @@
                <span class="flex-1 ms-3 whitespace-nowrap">Cerrar sesión</span>
             </a>
          </li>
+         -------------->
       </ul>
    </div>
 </aside>
